1625e7ae191655a6087ff6d84f9bdf451f8bda7a
Andecochea <andecomaf@gmail.com>
2024-11-29 20:04:50 +0100

    Ejercicio3

---
+++ Respuesta3_AndecocheaMiguel/Comprobacion-3-A/Persona.java
@@ -0,0 +1,14 @@
+public class Persona {
+    public String nombre;
+    public String dni;
+
+    public Persona(String nombre, String dni) {
+        this.nombre = nombre;
+        this.dni = dni;
+    }
+
+    public Persona(Persona persona) {
+        this.nombre = persona.nombre;
+        this.dni = persona.dni;
+    }
+}
--- Respuesta3_AndecocheaMiguel/Comprobacion-3-A/Principal.java
+++ Respuesta3_AndecocheaMiguel/Comprobacion-3-A/Principal.java
@@ -1,45 +1,3 @@
-class Nodo {
-    public Persona persona;
-    public Nodo siguiente;
-
-    public Nodo(Persona persona, Nodo siguiente) {
-        this.persona = persona;
-        this.siguiente = siguiente;
-    }
-}
-
-class Lista {
-    public Nodo primero;
-
-    public Lista() {
-        this.primero = null;
-    }
-
-    public void insertarPersona(Persona persona) {
-        Nodo nuevo = new Nodo(persona, primero);
-        primero = nuevo;
-    }
-
-    public Nodo getCabeza() {
-        return primero;
-    }
-}
-
-class Persona {
-    public String nombre;
-    public String dni;
-
-    public Persona(String nombre, String dni) {
-        this.nombre = nombre;
-        this.dni = dni;
-    }
-
-    public Persona(Persona persona) {
-        this.nombre = persona.nombre;
-        this.dni = persona.dni;
-    }
-}
-
 public class Principal {
     public static void main(String[] args) {
         Persona manuel = new Persona("Manuel", "1234");
@@ -59,4 +17,3 @@ public class Principal {
         }
     }
 }
-
